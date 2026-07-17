12d4b91d5823b6d5e31b4952359c1ea32133c4b0
tengjinyu <tjy_ateng@163.com>
2026-07-17 15:05:31 +0800

    fix: 移除自动打开devtools

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,9 +4,7 @@ use tauri::Manager;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let window = app.get_webview_window("main").unwrap();
-            window.open_devtools();
+        .setup(|_app| {
             Ok(())
         })
         .run(tauri::generate_context!())
