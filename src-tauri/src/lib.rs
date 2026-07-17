08f5fd65e02abbb566da3edce3cba3aa43b169ef
tengjinyu <tjy_ateng@163.com>
2026-07-17 18:21:20 +0800

    feat: 工作台优化、待办事项、快捷入口、操作记录优化

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ use tauri::Manager;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|_app| {
             Ok(())
         })
